c1359f79d5de64fe19b9aa3f8453a532ee46f4b5
Maharshi Pandya <maharshi.pandya2002@gmail.com>
2022-04-20 12:28:21 +0530

    :hammer: updates to frontend

--- os lab frontend/responsive-website-design-homepage/js/simulate.js
+++ os lab frontend/responsive-website-design-homepage/js/simulate.js
@@ -89,5 +89,37 @@ sim_form.addEventListener('submit', (event) => {
         }
     })
         .then(response => response.json())
-        .then(res => console.log(res));
+        .then(res => {
+            const labels = [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+            ];
+
+            const data = {
+                labels: labels,
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                }]
+            };
+
+            const config = {
+                type: 'line',
+                data: data,
+                options: {}
+            };
+
+            console.log(res);
+
+            const myChart = new Chart(
+                document.getElementById('myChart'),
+                config
+            );
+        });
 });
